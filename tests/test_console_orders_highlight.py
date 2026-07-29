d16212006475ec681d7551bfcdb6aae6d3384734
Glen Swartwout <drglenswartwout@gmail.com>
2026-07-29 11:57:31 -1000

    fix: show order actions above products

--- tests/test_console_orders_highlight.py
+++ tests/test_console_orders_highlight.py
@@ -61,3 +61,19 @@ def test_easypost_tracking_can_be_emailed_without_retyping():
     assert "o.tracking_number ? btn(o.id,'orders.send_tracking','Send tracking')" in html
     assert "HH.byId[Number(id)].tracking_number" in html
     assert "prompt('Tracking number (optional):', saved)" in html
+
+
+def test_expanded_cards_put_actions_above_products():
+    html = _html()
+    actions = "+ '<div class=\"acts\">'+acts+'</div>'"
+    products = "+ (items?'<div class=\"meta prods\">'+items+'</div>':'')"
+
+    order_body = html.split("var body = '<div class=\"card-body\">'", 1)[1].split(
+        "return '<div class=\"card\" data-oid=", 1
+    )[0]
+    shipment_body = html.split("var body = '<div class=\"card-body\">'", 2)[2].split(
+        "return '<div class=\"card hh-ship\"", 1
+    )[0]
+
+    assert order_body.index(actions) < order_body.index(products)
+    assert shipment_body.index(actions) < shipment_body.index(products)
