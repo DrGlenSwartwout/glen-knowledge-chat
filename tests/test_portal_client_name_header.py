c6e4640d1c9756109c67a60e03777bfbc5bc38e1
Glen Swartwout <drglenswartwout@gmail.com>
2026-07-30 01:55:25 -1000

    Prefer reviewed client name in portal header

--- tests/test_portal_client_name_header.py
+++ tests/test_portal_client_name_header.py
@@ -18,7 +18,7 @@ def test_client_name_header_is_at_top_of_portal():
 
 def test_client_name_header_uses_account_name_without_email_fallback():
     assert (
-        'const portalClientName = (v && v.account && v.account.name) || d.name || "";'
+        'const portalClientName = d.name || (v && v.account && v.account.name) || "";'
         in HTML
     )
     assert 'identityName.textContent = safeClientName;' in HTML
